2b3b3b8079cb891869d81d3be2529c387f215f7e
pedrovfpprogram <pedrovfpprogram@yahoo.com>
2026-06-10 16:17:01 -0300

    ate a 6

---
+++ 7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "potencia.hpp"
+
+int main(){
+
+    char N[] = "153";
+    cin >> N;
+
+
+    return 0;
+}
---
+++ potencia.hpp
@@ -0,0 +1,4 @@
+#ifndef POTENCIA_HPP
+#define POTENCIA_HPP
+int potencia(int base, int expoente);
+#endif
